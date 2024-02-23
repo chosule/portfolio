f17c50126f256e766fda7df6bde40e8450c0eddc
chosule <89799325+chosule@users.noreply.github.com>
2024-02-23 20:14:17 +0900

    🛠 : Type 수정하기

--- src/components/ui/StackWrap.tsx
+++ src/components/ui/StackWrap.tsx
@@ -1,25 +1,25 @@
+export type StackBoxProps = {
+  text: string;
+  color: string;
+  bgColor: string;
+};
 
-
-export type StackBoxProps  = {
-    text:string;
-    color:string;
-    bgColor:string;
-}
-
-type StackWrapProps<T extends StackBoxProps> = {
-    stackItems: T[];
-}
-
-export default function StackWrap<T extends StackBoxProps>({stackItems}:StackWrapProps<T>) {
-    
-     return(
-        <div className="flex gap-3 flex-wrap">
-            {stackItems.map((item,index) =>(
-                <div key={index}>
-                    <p className={`${item.color} ${item.bgColor} py-1 rounded-lg text-center text-[19px] px-2 font-medium md:text-[12px]`}>{item.text}</p>
-                </div>
-            ))}
+export default function StackWrap({
+  stackItems,
+}: {
+  stackItems: StackBoxProps[];
+}) {
+  return (
+    <div className="flex gap-3 flex-wrap">
+      {stackItems.map((item, index) => (
+        <div key={index}>
+          <p
+            className={`${item.color} ${item.bgColor} py-1 rounded-lg text-center text-[19px] drop-shadow-md px-2 font-medium md:text-[12px]`}
+          >
+            {item.text}
+          </p>
         </div>
-     )
+      ))}
+    </div>
+  );
 }
-
